da09afd9a09a4f9062e0c4f4c3c5c9b746638ad5
jHilscher <joerg.hilscher@gmail.com>
2013-11-04 13:49:36 +0100

    working authservice, added rest-method

---
+++ LockDataService/Model/Repository/MockRepository.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using LockDataService.Model.Entity;
+
+namespace LockDataService.Model.Repository 
+{
+    /// <summary>
+    /// Mock Repository with DB-Access.
+    /// </summary>
+    public class MockRepository : IRepository
+    {
+
+        public static MockEntities Entities = new MockEntities();
+
+        public class MockEntities
+        {
+            public List<ClientIdentifier> ClientIdentifier = new List<ClientIdentifier>();
+
+            public int SaveChanges()
+            {
+                return 1;
+            }
+        }
+
+        public int CreateUser(UserModel userModel)
+        {
+            // check
+            if (Entities.ClientIdentifier.FirstOrDefault(x => x.UserName == userModel.UserName) != null)
+            {
+                return UpdateUser(userModel);
+            }
+
+            userModel.DateTimeCreated = DateTime.Now;
+            Entities.ClientIdentifier.Add(ConvertToClientIdentifier(userModel));
+
+            return Entities.SaveChanges();
+        }
+
+        public UserModel GetUserById(int id)
+        {
+            return ConvertToUserModel(Entities.ClientIdentifier.FirstOrDefault(x => x.Id == id));
+        }
+
+        public UserModel GetUserByUserName(string userName)
+        {
+            UserModel userModel = ConvertToUserModel(Entities.ClientIdentifier.FirstOrDefault(x => x.UserName == userName));
+
+            return userModel;
+        }
+
+        public List<UserModel> GetAll()
+        {
+            return Entities.ClientIdentifier.ToList().Select(ConvertToUserModel).ToList();
+        }
+
+        public int UpdateUser(UserModel userModel)
+        {
+            ClientIdentifier clientIdentifier = Entities.ClientIdentifier.FirstOrDefault(x => x.UserName == userModel.UserName);
+
+            if (clientIdentifier != null)
+            {
+                clientIdentifier.LastLogin = DateTime.Now;
+                clientIdentifier.HashedClientId = userModel.HashedClientId;
+                clientIdentifier.Salt = userModel.Salt;
+                clientIdentifier.Secret = userModel.Secret;
+            }
+
+
+            return Entities.SaveChanges();
+        }
+
+        public int DeleteUser(string userName)
+        {
+            ClientIdentifier cId = Entities.ClientIdentifier.FirstOrDefault(x => x.UserName.Equals(userName));
+            Entities.ClientIdentifier.Remove(cId);
+            return Entities.SaveChanges();
+        }
+        
+#region converter
+
+        private static ClientIdentifier ConvertToClientIdentifier(UserModel userModel)
+        {
+            if (userModel == null)
+                return null;
+
+            return new ClientIdentifier
+                {
+                    HashedClientId = userModel.HashedClientId,
+                    Salt = userModel.Salt,
+                    Secret = userModel.Secret,
+                    DateCreated = userModel.DateTimeCreated,
+                    LastLogin = userModel.DateTimeLogin,
+                    UserName = userModel.UserName
+                };
+        }
+
+        private static UserModel ConvertToUserModel(ClientIdentifier clientIdentifier)
+        {
+            if (clientIdentifier == null)
+                return null;
+
+            return new UserModel
+            {
+                HashedClientId = clientIdentifier.HashedClientId,
+                Salt = clientIdentifier.Salt,
+                Secret = clientIdentifier.Secret,
+                DateTimeCreated = clientIdentifier.DateCreated,
+                DateTimeLogin = clientIdentifier.LastLogin,
+                UserName = clientIdentifier.UserName
+            };
+        }
+
+#endregion
+
+    }
+}
---
+++ LockDataService/Service/AuthHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Web;
+
+
+namespace LockDataService.Service
+{
+    public class AuthHandler
+    {
+        private static readonly Dictionary<string, string> Tokens = new Dictionary<string, string>();
+
+        private const int Timeframe = 1000*60*2; // 2mins
+
+
+        public static void AddToWaitList(string userName, string token)
+        {
+            token = token.ToUpper();
+
+            // will override old tokens
+            Tokens[userName] = token;
+
+            // Start Timer
+            //Timer removeTimer = new Timer();
+            //removeTimer.Interval = Timeframe;
+            //removeTimer.Elapsed += (sender, e) => RemoveFromWaitlist(sender, e, userName);
+            //removeTimer.Enabled = true;
+
+        }
+
+        private static void RemoveFromWaitlist(object sender, ElapsedEventArgs e, string userName)
+        {
+            Tokens.Remove(userName);
+        }
+
+        public static string GetToken(string userName)
+        {
+            return Tokens[userName];
+        }
+
+        public static string GetUser(string token)
+        {
+            token = token.ToUpper();
+            return Tokens.FirstOrDefault(x => x.Value.Equals(token)).Key;
+        }
+    }
+}
